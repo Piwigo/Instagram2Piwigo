957104ec5e8e6bdb8262d6bdfb65e55eea186941
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-04 21:42:25 +0000

    [instagram2piwigo] Add ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/instagram2piwigo@19809 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['How do I get my Instagram Client ID?'] = 'Com puc obtenir el meu «Client ID» d\'Instagram?';
+$lang['Import'] = 'Importar';
+$lang['Import options'] = 'Opcions d\'importació';
+$lang['Instagram logins'] = 'Inici de sessió d\'Instagram';
+$lang['List my pictures'] = 'Llistar les meves fotos';
+$lang['Logged in as'] = 'Autenticat com';
+$lang['Logged out'] = 'Sessió finalitzada';
+$lang['No download method available'] = 'Cap mètode de descàrrega disponible';
+$lang['One picture is not displayed because already existing in the database.'] = 'Una foto no és mostrada perquè ja existeix a la base de dades.';
+$lang['Open Instagram page in a new tab'] = 'Obriu la pàgina d\'Instagram en una nova pestanya';
+$lang['Photo "%s" imported'] = 'Imatge "%s" importada';
+$lang['Please fill your API keys on the configuration tab'] = 'Ompliu les vostres claus API a la pestanya de configuració';
+$lang['Processing...'] = 'Processant...';
+$lang['Successfully logged in to you Instagram account'] = 'Us heu connectat correctament al vostre compte d\'Instagram';
+$lang['%d albums'] = '%d àlbums';
+$lang['%d pictures are not displayed because already existing in the database.'] = 'No es mostren %d fotos perquè ja existeixen a la base de dades.';
+$lang['%d pictures imported'] = '%d fotos importades';
+$lang['(%d photos)'] = '(%d fotos)';
+$lang['API not authenticated'] = 'API no autenticada';
+$lang['Begin transfer'] = 'Inicieu la transferència';
+$lang['Fill these fields from Instagram datas'] = 'Ompliu aquests camps de dades d\'Instagram';
+?>
