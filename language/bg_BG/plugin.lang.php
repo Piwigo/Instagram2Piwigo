a5a8cf1943b404c050be0a229174c4a62d70fafc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-19 10:27:16 +0000

    [instagram2piwigo] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/instagram2piwigo@20843 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Successfully logged in to you Instagram account'] = 'Успешно се свързахте с вашия Instagram акаунт';
+$lang['Processing...'] = 'Обработване...';
+$lang['Please fill your API keys on the configuration tab'] = 'Моля въведете вашият API ключ в конфигурационния прозорец';
+$lang['Photo "%s" imported'] = 'Снимки "%s" импортирани';
+$lang['Open Instagram page in a new tab'] = 'Отваряне на Instagram страницата в нов таб';
+$lang['One picture is not displayed because already existing in the database.'] = 'Една снимка не е показана защото вече съществува в базата данни.';
+$lang['No download method available'] = 'Няма възможна опция за сваляне';
+$lang['Logged out'] = 'Излязохте';
+$lang['Logged in as'] = 'Влезли сте като';
+$lang['List my pictures'] = 'Моя списък снимки';
+$lang['Instagram logins'] = 'Instagram logins';
+$lang['Import options'] = 'Опции за импортиране';
+$lang['Import'] = 'Импорт';
+$lang['How do I get my Instagram Client ID?'] = 'Как да получа моя Instagram клиент номер?';
+$lang['Fill these fields from Instagram datas'] = 'Попълнете тези полета от Instagram ';
+$lang['Begin transfer'] = 'Начало на преместване';
+$lang['API not authenticated'] = 'API не са удостоверени';
+$lang['(%d photos)'] = '(%d снимки)';
+$lang['%d pictures imported'] = '%d вмъкнати снимки';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d снимки не са показани защото вече съществуват в базата данни.';
+$lang['%d albums'] = '%d албуми';
+?>
