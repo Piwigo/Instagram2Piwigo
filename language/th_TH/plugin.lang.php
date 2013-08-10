5e2e2e97359f66accba58205cfc2046f94a8c1ff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-10 17:49:27 +0000

    [instagram2piwigo] Add th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/instagram2piwigo@24147 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Successfully logged in to you Instagram account'] = 'การเข้าสู่ระบบเรียบร้อยแล้ว กำลังล็อกอินไปยังคุณบัญชี Instagram';
+$lang['%d pictures imported'] = '%d ภาพที่ถูกนำเข้า';
+$lang['(%d photos)'] = '%d ภาพ';
+$lang['API not authenticated'] = 'API ไม่ได้รับรองอนุญาต';
+$lang['Begin transfer'] = 'เริ่มต้นการถ่ายโอน';
+$lang['Fill these fields from Instagram datas'] = 'กรอกข้อมูลเหล่านี้จากข้อมูล Instagram';
+$lang['How do I get my Instagram Client ID?'] = 'ฉันจะได้รับ ID สำหรับใช้งาน Instagram ได้อย่างไร';
+$lang['Import'] = 'นำเข้า';
+$lang['Import options'] = 'ตัวเลือกการนำเข้า';
+$lang['Instagram logins'] = 'เข้าสู่ระบบ Instagram';
+$lang['List my pictures'] = 'รายการภาพของฉัน';
+$lang['Logged in as'] = 'เข้าสู่ระบบเป็น';
+$lang['Logged out'] = 'ออกจากระบบ';
+$lang['One picture is not displayed because already existing in the database.'] = 'หนึ่งในภาพที่ถูกแสดงเพราะมีอยู่แล้วในฐานข้อมูล';
+$lang['Open Instagram page in a new tab'] = 'เปิดหน้า Instagram ในแท็บใหม่';
+$lang['Photo "%s" imported'] = 'ภาพ "%s" ที่ถูกนำเข้า';
+$lang['Please fill your API keys on the configuration tab'] = 'กรุณากรอกคีย์ API ของคุณบนแท็บการตั้งค่า';
+$lang['Processing...'] = 'ประมวลผล ...';
+$lang['%d albums'] = '%d อัลบั้ม';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d ภาพ จะไม่แสดงเพราะมีอยู่แล้วในฐานข้อมูล';
+?>
