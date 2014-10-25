7278f8b3ee17b1ec67c78470b3955b840bcf168d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-25 09:50:32 +0000

    [instagram2piwigo] Add nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/instagram2piwigo@30303 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Successfully logged in to you Instagram account'] = 'Vellykket pålogget din Instagram-konto';
+$lang['%d albums'] = '%d albumer';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d bilder blir ikke vist fordi de finnes allerede i databasen.';
+$lang['%d pictures imported'] = '%d bilder importert';
+$lang['(%d photos)'] = '(%d bilder)';
+$lang['API not authenticated'] = 'API ikke autentisert';
+$lang['Begin transfer'] = 'Begynn overføringen';
+$lang['Fill these fields from Instagram datas'] = 'Fylle disse feltene med Instagram data';
+$lang['How do I get my Instagram Client ID?'] = 'Hvordan får jeg tak på min Instagram klient-ID?';
+$lang['Import'] = 'Import';
+$lang['Import options'] = 'Import valg';
+$lang['Instagram logins'] = 'Instagram innlogginger';
+$lang['List my pictures'] = 'Liste mine bilder';
+$lang['Logged in as'] = 'Logget inn som';
+$lang['Logged out'] = 'Logget ut';
+$lang['No download method available'] = 'Ingen nedlastnings metoder er tilgjengelig';
+$lang['One picture is not displayed because already existing in the database.'] = 'Et bilde blir ikke vist fordi det finnes allerede i databasen.';
+$lang['Open Instagram page in a new tab'] = 'Åpne Instagram siden i en ny fane';
+$lang['Photo "%s" imported'] = 'Bilde  "%s" importert';
+$lang['Please fill your API keys on the configuration tab'] = 'Vennligst fyll inn API nøkkelen i konfigurasjon fanen';
+$lang['Processing...'] = 'Behandler ...';
